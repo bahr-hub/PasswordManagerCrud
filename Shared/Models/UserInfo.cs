0314c3f7a0b31ee9ab93bc1c07769233a108cb72
Bahr <a.ah.ismail@gmail.com>
2022-09-24 14:55:05 +0200

    Add project files.

---
+++ Shared/Models/UserInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManagerCrud.Shared.Models
+{
+    public class UserInfo
+    {
+        public int UserPk { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+
+        public DateTime CreationDate { get; set; }
+        public long CreationDateStamp { get; set; }
+
+
+        public DateTime? UpdateDate { get; set; }
+        public long UpdateDateStamp { get; set; }
+
+
+        public int DeleteBy { get; set; }
+        public DateTime? DeleteDate { get; set; }
+        public long DeleteDateStamp { get; set; }
+    }
+}
